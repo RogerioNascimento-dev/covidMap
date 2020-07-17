49c340deb45583ce2f443b121c26684e7736a18e
Rogério Nascimento <rogerionascimento.dev@outlook.com.br>
2020-07-17 03:25:00 -0300

    ajuste no layout

--- src/pages/Single/styles.js
+++ src/pages/Single/styles.js
@@ -4,7 +4,8 @@ const styles = StyleSheet.create({
     container:{
         flex:1,
         alignItems: 'center',
-        justifyContent: 'center',        
+        justifyContent: 'center',  
+        backgroundColor:'#FFF',      
 
     },
     containerData:{      
