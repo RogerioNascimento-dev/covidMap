1bff4428b39e5efaded151a1dbc64ef38771bd8a
Rogério Nascimento <rogerio.nascimento@soterotech.com.br>
2021-10-26 02:29:56 -0300

    view files project

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,100 +1,131 @@
-import React,{useState,useEffect} from 'react';
-import { View,Text,Image,TextInput,TouchableOpacity } from 'react-native';
-import MapView,{Marker,Callout} from 'react-native-maps';
-import {useNavigation} from '@react-navigation/native'
-import { AntDesign,MaterialCommunityIcons,FontAwesome } from '@expo/vector-icons'; 
-import styles from './styles';
-import api from '../../services/api'
-import {currentPositionCountry,cordinates} from '../../commons/cordinates';
+import React, { useState, useEffect } from "react";
+import { View, Text, Image, TextInput, TouchableOpacity } from "react-native";
+import MapView, { Marker, Callout } from "react-native-maps";
+import { useNavigation } from "@react-navigation/native";
+import {
+  AntDesign,
+  MaterialCommunityIcons,
+  FontAwesome,
+} from "@expo/vector-icons";
+import styles from "./styles";
+import api from "../../services/api";
+import { currentPositionCountry, cordinates } from "../../commons/cordinates";
 
-const Home = () => {   
-  const navigation = useNavigation();  
-  const [dataApi,setDataApi] = useState([]);   
-  const [states,setStates] = useState([]);   
-  const [textFilter,setTextFilter] = useState('');
-  
-  function handleRegionChange(region){
+const Home = () => {
+  const navigation = useNavigation();
+  const [dataApi, setDataApi] = useState([]);
+  const [states, setStates] = useState([]);
+  const [textFilter, setTextFilter] = useState("");
+
+  function handleRegionChange(region) {
     console.log(region);
   }
 
-  useEffect(()=>{    
-    async function loadStates(){
-      const response = await api.get('/api/report/v1');   
-      const {data} = response.data;
-      data.map(es =>{
-        cordinates.map(cd =>{
-            if(cd.uf === es.uf){
-                es.coordenadas = {latitude:cd.latitude,longitude:cd.longitude}
-            }
-        })
-      })    
+  useEffect(() => {
+    async function loadStates() {
+      const response = await api.get("/api/report/v1");
+      const { data } = response.data;
+      data.map((es) => {
+        cordinates.map((cd) => {
+          if (cd.uf === es.uf) {
+            es.coordenadas = { latitude: cd.latitude, longitude: cd.longitude };
+          }
+        });
+      });
       setDataApi(data);
       setStates(data);
-    }    
+    }
     loadStates();
-  },[])
+  }, []);
 
-  const handleButtonFilter = () =>{    
-    
-    if(textFilter.trim() === ""){
+  const handleButtonFilter = () => {
+    if (textFilter.trim() === "") {
       setStates(dataApi);
       return;
     }
 
-    let statesFilter = textFilter.split(',');
+    let statesFilter = textFilter.split(",");
     let statesFiltered = [];
-    statesFilter.map((obj) =>{
-      let dataApiFiltered = dataApi.filter((dado)=>{ 
-        return obj == dado.uf;  
-      })
-      statesFiltered.push(...dataApiFiltered)
-    })
-    setStates(statesFiltered);  
-  }
+    statesFilter.map((obj) => {
+      let dataApiFiltered = dataApi.filter((dado) => {
+        return obj == dado.uf;
+      });
+      statesFiltered.push(...dataApiFiltered);
+    });
+    setStates(statesFiltered);
+  };
 
   return (
-    <>       
-        <MapView         
+    <>
+      <MapView
         style={styles.map}
         /*onRegionChangeComplete={handleRegionChange}*/
-        initialRegion={currentPositionCountry}>          
-          {states.map(state =>(
-            <Marker key={`${state.uid}`} coordinate={state.coordenadas}>
-            <Image style={styles.marker} source={{uri:`https://devarthurribeiro.github.io/covid19-brazil-api/static/flags/${state.uf}.png`}} />
-            <Callout onPress={() =>{
-              navigation.navigate('Single',{state:state.uf})
-            }}>                          
+        initialRegion={currentPositionCountry}
+      >
+        {states.map((state) => (
+          <Marker key={`${state.uid}`} coordinate={state.coordenadas}>
+            <Image
+              style={styles.marker}
+              source={{
+                uri: `https://devarthurribeiro.github.io/covid19-brazil-api/static/flags/${state.uf}.png`,
+              }}
+            />
+            <Callout
+              onPress={() => {
+                navigation.navigate("Single", { state: state.uf });
+              }}
+            >
               <View style={styles.callout}>
-                  <Text style={styles.stateName}>{state.state}</Text>
-                  <Text style={styles.cases}><AntDesign name="frowno" size={15} color="#9fb5c8" /> {state.cases} Casos confirmados</Text>
-                  <Text style={styles.cases}><MaterialCommunityIcons name="skull" size={15} color="#9fb5c8" /> {state.deaths} Óbitos confirmados</Text>
-                  <View style={{alignItems:'center',justifyContent:'center',flexDirection:'row'}}>
-                    <Text style={styles.cases}>Clique para ver mais</Text>
-                    <FontAwesome name="arrow-right" size={15} color="red" />
-                  </View>
+                <Text style={styles.stateName}>{state.state}</Text>
+                <Text style={styles.cases}>
+                  <AntDesign name="frowno" size={15} color="#9fb5c8" />{" "}
+                  {state.cases} Casos confirmados
+                </Text>
+                <Text style={styles.cases}>
+                  <MaterialCommunityIcons
+                    name="skull"
+                    size={15}
+                    color="#9fb5c8"
+                  />{" "}
+                  {state.deaths} Óbitos confirmados
+                </Text>
+                <View
+                  style={{
+                    alignItems: "center",
+                    justifyContent: "center",
+                    flexDirection: "row",
+                  }}
+                >
+                  <Text style={styles.cases}>Clique para ver mais</Text>
+                  <FontAwesome name="arrow-right" size={15} color="red" />
+                </View>
               </View>
             </Callout>
           </Marker>
-          ))}      
-        </MapView>
+        ))}
+      </MapView>
 
-        <View style={styles.containerFilter}>
-          <TextInput 
+      <View style={styles.containerFilter}>
+        <TextInput
           placeholder="Filtrar Ex: SP,BA,AL..."
           placeholderTextColor="#FFB0B5"
           autoCapitalize="characters"
-          autoCorrect={false}          
+          autoCorrect={false}
           onChangeText={(valor) => setTextFilter(valor)}
           style={styles.inputFilter}
           returnKeyType="search"
-          onSubmitEditing={handleButtonFilter} />  
-        
-          <TouchableOpacity style={styles.buttonFilter} onPress={handleButtonFilter}>
-              <FontAwesome color="red" size={20} name="filter" />
-          </TouchableOpacity>           
-        </View>  
-    </>        
+          onSubmitEditing={handleButtonFilter}
+        />
+
+        <TouchableOpacity
+          style={styles.buttonFilter}
+          onPress={handleButtonFilter}
+        >
+          <FontAwesome color="red" size={20} name="filter" />
+        </TouchableOpacity>
+      </View>
+    </>
   );
-}
+};
 
 export default Home;
--- src/pages/Home/styles.js
+++ src/pages/Home/styles.js
@@ -1,54 +1,54 @@
-import {StyleSheet,  Dimensions} from 'react-native'
+import { StyleSheet, Dimensions } from "react-native";
 
-const styles = StyleSheet.create({    
-    map:{
-        flex:1,      
+const styles = StyleSheet.create({
+    map: {
+        flex: 1,
     },
-    marker:{
-        width:45,
-        height:30,        
+    marker: {
+        width: 45,
+        height: 30,
     },
-    callout:{
-        width:230,
-    },    
-    stateName:{
-        color:'#FF101F',
-        fontWeight:'bold',
-        fontSize:18
+    callout: {
+        width: 230,
     },
-    cases:{
-        fontSize:15,
-        fontWeight:'bold',
-        color:'#9fb5c8',     
-        marginRight:5,
-    },  
-    inputFilter:{
-        backgroundColor:'#FFF',
-        height:40,
-        width:Dimensions.get('window').width * 0.6,                
-        paddingHorizontal:10,
-        borderWidth:1,    
-        borderColor:'#FFB0B5',
-        borderRadius:20,                 
-    },  
-    containerFilter:{
+    stateName: {
+        color: "#FF101F",
+        fontWeight: "bold",
+        fontSize: 18,
+    },
+    cases: {
+        fontSize: 15,
+        fontWeight: "bold",
+        color: "#9fb5c8",
+        marginRight: 5,
+    },
+    inputFilter: {
+        backgroundColor: "#FFF",
+        height: 40,
+        width: Dimensions.get("window").width * 0.6,
+        paddingHorizontal: 10,
+        borderWidth: 1,
+        borderColor: "#FFB0B5",
+        borderRadius: 20,
+    },
+    containerFilter: {
         flexDirection: "row",
-        position:'absolute',        
+        position: "absolute",
         bottom: 20,
-        left: Dimensions.get('window').width * 0.15,
-        alignItems:'center',        
-    },    
-    buttonFilter:{
-        backgroundColor:'#FFF',
-        borderColor:'#FFB0B5',
-        borderWidth:1,
-        width:40,
-        height:40,   
-        borderRadius:20,     
-        alignItems:'center',
-        justifyContent:'center',
-        marginLeft:10,
-    }
+        left: Dimensions.get("window").width * 0.15,
+        alignItems: "center",
+    },
+    buttonFilter: {
+        backgroundColor: "#FFF",
+        borderColor: "#FFB0B5",
+        borderWidth: 1,
+        width: 40,
+        height: 40,
+        borderRadius: 20,
+        alignItems: "center",
+        justifyContent: "center",
+        marginLeft: 10,
+    },
 });
 
 export default styles;
